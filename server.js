113149437017992f12d54b229983f53e61bfc177
@ignacio <98288385+Ignacivss@users.noreply.github.com>
2024-06-23 20:39:39 -0400

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const server = express();
- 
-function keepAlive() {
-  server.listen(3000, () => { console.log("asumakina we " + Date.now()) });
-}
- 
-module.exports = keepAlive;
